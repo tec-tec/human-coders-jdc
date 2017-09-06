543f3d950c1f4b85bbc06a432946492fe5323083
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-06 11:55:09 +0200

    Ajout d'une classe pour le ViewController avec le tableview

---
+++ Projets/Awesome Restaurant/Awesome Restaurant/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Awesome Restaurant
+//
+//  Created by Ludovic Ollagnier on 06/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
