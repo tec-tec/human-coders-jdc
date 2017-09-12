0078a291bd290860883931c4700080f5a93f7205
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-12 14:01:26 +0200

    Ajout projet Swift demo

---
+++ Projets/Swift project/Swift project/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Swift project
+//
+//  Created by Ludovic Ollagnier on 11/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Outlets
+
+    //MARK: Public vars
+
+    //MARK: Private vars
+
+
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - Public methods
+
+    //MARK: - Private methods
+
+}
+
